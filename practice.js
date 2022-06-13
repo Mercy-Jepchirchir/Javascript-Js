12e195c12adfd961deeff3f7562171a336de2e2f
Mercy-Jepchirchir <mjepchirchirkemboi@gmail.com>
2022-06-13 22:15:06 +0300

    first commit

---
+++ practice.js
@@ -0,0 +1,59 @@
+// variable is a container that hold data
+let num = 5
+let x;
+x = 5;// assigning using assignment operator
+
+// 5 is assigned to variable x
+let X = 5; 
+console.log(x); // 5
+
+// vaue of variable x is changed
+x = 3; 
+console.log(x); // 3
+
+//const is a key word created - representing constant
+const y = 5;
+// y = 10        // error constant cannot be changed.
+console.log(y);
+
+const her = "Mercy";
+const him =  "Boniface";
+console.log(her +' ' +him)// concatinating strings
+
+//strings example
+const name = 'Mercy';
+const name1 = "chirii";
+const result = `The names are ${name} and ${name1}`;
+console.log(result);
+
+
+let m = 5;
+let z = 3;
+
+// addition
+console.log('m + z = ', x + y);  // 8
+
+// subtraction
+console.log('m - z = ', x - y);  // 2
+
+// multiplication
+console.log('m * z = ', x * y);  // 15
+
+// division
+console.log('m / z = ', x / y);  // 1.6666666666666667
+
+// remainder
+console.log('m % z = ', x % y);   // 2
+
+// increment
+console.log('++m = ', ++m); // x is now 6
+console.log('m++ = ', m++); // prints 6 and then increased to 7
+console.log('m = ', m);     // 7
+
+// decrement
+console.log('--m = ', --m); // x is now 6
+console.log('m-- = ', m--); // prints 6 and then decreased to 5
+console.log('m = ', m);     // 5
+
+//exponentiation
+console.log('x ** y =', m ** z);
